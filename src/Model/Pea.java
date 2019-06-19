447c56034d5452939681b526ac1ca2488b0ebd9f
Richard Calderan <richardcck@hotmail.com>
2019-06-19 17:09:43 -0300

    Uma caralhada de mudanças
    Bug: Bomberman não está morrendo com explosão.
    
    Adicionado pasta SPRITES para usar
    Foram ripadas do jogo

---
+++ src/Model/Pea.java
@@ -0,0 +1,54 @@
+package Model;
+
+import Auxiliar.Consts;
+import Auxiliar.Draw;
+
+/**
+ * Represents a pea that can be shooted
+ */
+public class Pea extends Character {
+    private int lifeTime;
+    private int timer;
+    public Pea(String sNomeImagePNG, Consts.DIRECTION direction) {
+        super(sNomeImagePNG);
+        this.setDirection(direction);
+        timer=0;
+        bMortal=true;
+        lifeTime=0;
+    }
+
+    public void switchAppearance() {
+
+        changeAppearance(getCharacterName() +"-d.png");
+    }
+
+    public int getLifeTime() {
+        return lifeTime;
+    }
+
+    @Override
+    public void autoDraw() {
+        timer++;
+        if(timer>Consts.PERIOD/20) {//control its velocity
+            switch (getDirection()) {
+                case DOWN:
+                    moveDown();
+                    break;
+                case UP:
+                    moveUp();
+                    break;
+                case LEFT:
+                    moveLeft();
+                    break;
+                case RIGHT:
+                    moveRight();
+                    break;
+            }
+            timer=0;
+            lifeTime++;
+        }
+        if(lifeTime>=5)
+            die();
+        Draw.draw(this.iImage, pPosition.getColumn(), pPosition.getLine());
+    }
+}
